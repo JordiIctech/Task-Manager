d97a87664fdc3a8a025f5b2f4a9a373b10ff4ff2
JTech <jordiictech@gmail.com>
2022-10-07 20:59:56 -0400

    Added images viaasset & url

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import logo from './assets/logo.png';
 
+//Logo is variable used imported from above. Image #1 in assets, #2 in url.
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ color: '#888', fontSize: 18 }}> 
-        To share a photo from your phone with a friend, just press the button below!
+      <Image source={logo} style={{ width: 305, height: 159 }} />  
+      <Text style={{color: '#888', fontSize: 18}}> 
+        To share a photo from your phone with a friend, just press the button below! 
       </Text>
+      <Image source={{ url: "https://i.imgur.com/TkIrScD.png" }} style={{ width: 305, height: 159 }} />
     </View>
   );
 }
-
+/*General page style*/ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
