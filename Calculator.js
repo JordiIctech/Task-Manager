f908477bff04496e3098e95281334507f3f4f8cd
JTech <jordiictech@gmail.com>
2022-10-26 21:24:50 -0400

    Added CSS styles

--- Calculator.js
+++ Calculator.js
@@ -1,13 +1,15 @@
 import * as React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet} from 'react-native';
+import {styles} from "./Stylesdef.js" /*This variable {styles} is then used in the view to define the function
+                                      that controls its template.*/
 
 export function CalculatorScreen({ navigation }) { //Screen 2
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        //The format for styles is "style = stylesfile.styleytype"
+      <View style={styles.button1}>
         <Text>Calculator!
           Calculator
         </Text>
-  
         <Button
           title="Go to Home"
           onPress={() => navigation.navigate('Home')}
---
+++ Stylesdef.js
@@ -0,0 +1,35 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    logo: {
+      width: 305,
+      height: 159,
+      marginBottom: 10,
+      marginTop: 10,
+    },
+    instructions: {
+      color: '#888',
+      fontSize: 18,
+      marginHorizontal: 15,
+    },
+    button1: {
+      backgroundColor: "lightblue",
+      padding: 20,
+      borderRadius: 5,
+    },
+    buttontext1: {
+      fontSize: 20,
+      color: '#fff',
+    },
+    importedI: {
+      width: 30,
+      height: 30,
+      resizeMode: "contain"
+    },
+  })
