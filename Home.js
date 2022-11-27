647cf1cee96ef178fcd89db68195db0b886354ba
JTech <jordiictech@gmail.com>
2022-11-26 21:22:19 -0500

    Home Screen: Home screen greatly improved.

--- Home.js
+++ Home.js
@@ -4,47 +4,49 @@ import {styles} from "./Stylesdef.js"
 
 export function HomeScreen({ navigation }) { //Screen 1, need to pass the navigation variable from the top.
     return (
-      <View style={styles.defaulted}>
-        <Text style={{fontSize: "25"}}>Home Screen!</Text>
-        
-        <TouchableOpacity style={{alignItems: 'center'}}
-        onPress={() => navigation.navigate('Testing')}>
-        <Image style={styles.homebutton} 
-          source={require('./assets/videoicon.png')} />
-          <Text style={{color: "red"}}>Test Environment</Text>
-        </TouchableOpacity>
+      <View style={{flex: 1, width:"100%", height: "100%"}}>
+      <Text style={{fontSize: "25", alignSelf:"center", marginTop: 10}}>Home Screen!</Text>
 
-        <TouchableOpacity style={{alignItems: 'center'}}
+
+    {/*First view contains page, second contains buttons*/}
+      <View style={styles.defaulted}>                        
+      
+        <TouchableOpacity style={styles.homeparentbutton}
         onPress={() => navigation.navigate('Calculator')}>
         <Image style={styles.homebutton} 
-          source={require('./assets/videoicon.png')} />
+          source={require('./assets/calculatoricon.png')} />
           <Text style={styles.hometext}>Calculator</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={{alignItems: 'center'}}
+        <TouchableOpacity style={styles.homeparentbutton}
         onPress={() => navigation.navigate('Tasks')}>
         <Image style={styles.homebutton} 
-          source={require('./assets/videoicon.png')} />
+          source={require('./assets/taskicon.png')} />
           <Text style={styles.hometext}>Task Manager</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={{alignItems: 'center'}}
+        <TouchableOpacity style={styles.homeparentbutton}
         onPress={() => navigation.navigate('Platformer')}>
         <Image style={styles.homebutton} 
-          source={require('./assets/videoicon.png')} />
+          source={require('./assets/gameicon.png')} />
           <Text style={styles.hometext}>Platformer</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={{alignItems: 'center'}}
+        <TouchableOpacity style={styles.homeparentbutton}
         onPress={() => navigation.navigate('Video')}>
         <Image style={styles.homebutton} 
           source={require('./assets/videoicon.png')} />
           <Text style={styles.hometext}>Video</Text>
         </TouchableOpacity>
 
-
-        
-  
+        <TouchableOpacity style={styles.homeparentbutton}
+        onPress={() => navigation.navigate('Testing')}>
+        <Image style={styles.homebutton} 
+          source={require('./assets/testingicon.png')} />
+          <Text style={{color: "red", fontSize:"20"}}>Test Environment</Text>
+        </TouchableOpacity>
+      
+      </View>
       </View>
     );
   }
