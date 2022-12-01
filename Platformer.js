6270a519214bf1113de3eb82221e87673e102b2d
JTech <jordiictech@gmail.com>
2022-12-01 03:53:01 -0500

    Game App: Basic troubleshooting, broken.

--- Platformer.js
+++ Platformer.js
@@ -4,21 +4,30 @@ import {styles} from "./Stylesdef.js"
 
 export function Platformer({ navigation }) { 
 
-  function position(SL, L, ST, T, Vel){ //Moves the character based on arrow click
-      SL(leftP += L*Vel)
-      ST(topP += T*Vel)
-  }
-//---------------------------------------------------- Unfinished
-useEffect(() => {
-  const interval = setInterval(() => {
-    console.log('Ten seconds have passed');
-  }, 10000);
-  return () => clearInterval(interval);
-}, []); // Not sure what [] does.
 
+//---------------------------------------------------- Unfinished
+
+function direction(SX, X, SY, Y){
+  SX(xchange = X)
+  SY(ychange = Y)
+}
+position(xchange,ychange)
+
+function position(Xchan,Ychan){ //Moves the character based on arrow click
+  useEffect(() => {
+    const interval = setInterval(() => {
+    setCountL(leftP += Xchan)
+    setCountT(topP += Ychan)}, 1000);
+    console.log('Ten seconds have passed');
+    return () => clearInterval(interval);
+  }, []); // Not sure what [] does.
+}
+    
 //---------------------------------------------------- Unfinished
   var [leftP, setCountL] = useState(0); 
   var [topP, setCountT] = useState(0); 
+  var [xchange, setCountX] = useState(0); 
+  var [ychange, setCountY] = useState(0); 
 
   var velocity = 10
 
@@ -49,56 +58,56 @@ useEffect(() => {
 
 
 {/*Straight Arrows*/}
-        <TouchableOpacity onPress={()=> position(setCountL,1,setCountT,0,velocity)}>
+        <TouchableOpacity onPress={()=> direction(setCountX,1,setCountY,0)}>
           <Image style={{position: "absolute", transform: [{rotate: '0deg'}], //Right
           left: 380, top: 720, width: 40, height: 40}} 
           source={require('./assets/arrowstraight.png')} />
         </TouchableOpacity>
 
-        <TouchableOpacity onPress={()=> position(setCountL,0,setCountT,1,velocity)}>
+        <TouchableOpacity onPress={()=> direction(setCountX,0,setCountY,1)}>
           <Image style={{position: "absolute", transform: [{rotate: '90deg'}], //Bottom
           left: 335, top: 765, width: 40, height: 40}} 
           source={require('./assets/arrowstraight.png')} />
         </TouchableOpacity>
 
-        <TouchableOpacity onPress={()=> position(setCountL,-1,setCountT,0,velocity)}>
+        <TouchableOpacity onPress={()=> direction(setCountX,-1,setCountY,0)}>
           <Image style={{position: "absolute", transform: [{rotate: '180deg'}], //Left
           left: 290, top: 720, width: 40, height: 40}} 
           source={require('./assets/arrowstraight.png')} />
         </TouchableOpacity>
 
-        <TouchableOpacity onPress={()=> position(setCountL,0,setCountT,-1,velocity)}>
+        <TouchableOpacity onPress={()=> direction(setCountX,0,setCountY,-1)}>
           <Image style={{position: "absolute", transform: [{rotate: '270deg'}], //Top
           left: 335, top: 675, width: 40, height: 40}} 
           source={require('./assets/arrowstraight.png')} />
         </TouchableOpacity>
 
-{/*Diagonal Arrows*/}
-        <TouchableOpacity onPress={()=> position(setCountL,-leftP,setCountT,-topP,1)}>
+{/*Stop Movement*/}
+        <TouchableOpacity onPress={()=> position(setCountX,0,setCountY,0)}>
           <Image style={{position: "absolute", transform: [{rotate: '0deg'}], //Icon
           left: 335, top: 720, width: 40, height: 40}} 
           source={require('./assets/icon.png')} />
         </TouchableOpacity>
-
-        <TouchableOpacity onPress={()=> position(setCountL,1,setCountT,1,velocity)}>
+{/*Diagonal Arrows*/}
+        <TouchableOpacity onPress={()=> direction(setCountX,1,setCountY,1)}>
           <Image style={{position: "absolute", transform: [{rotate: '0deg'}], //Bottom Right
           left: 380, top: 765, width: 40, height: 40}} 
           source={require('./assets/arrowdiagonal.png')} />
         </TouchableOpacity>
 
-        <TouchableOpacity onPress={()=> position(setCountL,-1,setCountT,1,velocity)}>
+        <TouchableOpacity onPress={()=> direction(setCountX,-1,setCountY,1)}>
           <Image style={{position: "absolute", transform: [{rotate: '90deg'}], //Bottom Left
          left: 290, top: 765, width: 40, height: 40}} 
           source={require('./assets/arrowdiagonal.png')} />
         </TouchableOpacity>
 
-        <TouchableOpacity onPress={()=> position(setCountL,-1,setCountT,-1,velocity)}>
+        <TouchableOpacity onPress={()=> direction(setCountX,-1,setCountY,-1)}>
           <Image style={{position: "absolute", transform: [{rotate: '180deg'}], //Top Left
           left: 290, top: 675, width: 40, height: 40}} 
           source={require('./assets/arrowdiagonal.png')} />
         </TouchableOpacity>
 
-        <TouchableOpacity onPress={()=> position(setCountL,1,setCountT,-1,velocity)}>
+        <TouchableOpacity onPress={()=> direction(setCountX,1,setCountY,-1)}>
           <Image style={{position: "absolute", transform: [{rotate: '270deg'}], //Top Right
           left: 380, top: 675, width: 40, height: 40}} 
           source={require('./assets/arrowdiagonal.png')} />
